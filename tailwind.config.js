fc60f3217411f87c0fa43a629d971f4354401ca8
Renner Costa <ozielrennerc.rodrigues@gmail.com>
2024-05-02 23:13:27 -0300

    fix: ajustando caminhos de arquivo no content em tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/**/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'bg-desktop-nav': "url('./src/assets/images/pattern-bg-desktop.png')",
-        'bg-mobile-nav': "url('./src/assets/images/pattern-bg-mobile.png')"
+        'bg-desktop-nav': "url('/pattern-bg-desktop.png')",
+        'bg-mobile-nav': "url('/pattern-bg-mobile.png')"
       }
     },
   },
